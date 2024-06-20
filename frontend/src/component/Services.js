8d5efb275c5bbd76fbcf16ce877c5e9623e66648
prakash-it <prakashkandasamy4@gmail.com>
2024-06-20 17:54:28 +0530

    IDA-creation

--- frontend/src/component/Services.js
+++ frontend/src/component/Services.js
@@ -11,10 +11,10 @@ import photo5 from './img/pro.png';
 import photo6 from './img/studio.png';
 import photo7 from './img/team.png';
 import photo8 from './img/ux.png';
-import photo9 from './img/vid.png';
 import photo10 from './img/visual.png';
 import photo11 from './img/vr.png';
-import photo12 from './img/work.ong';
+import photo12 from './img/work.png';
+import photo13 from './img/3d.png'
 
 export default function Services() {
   return (
@@ -24,8 +24,10 @@ export default function Services() {
 
   <div className="row mt-5">
     <div className="col-lg-12 text-center">
+      <div className='ser'>
     <h3 className='services'>Our Services</h3>
       <p>Reinvent your industry with our industrial solutions</p>
+      </div>
     </div>
   </div>
 </div>
@@ -34,19 +36,19 @@ export default function Services() {
   <div className="row p-lg-3 p-md-3 p-0">
     <div className="col-lg-4 col-md-12 p-md-2 hidden-b">
       <a href="#">
-        <img src="img/game-development_2029515.png" alt="" width="50px"/>
+        <img src={photo4} alt="" width="50px"/>
         <p>Game Development</p>
       </a>
     </div>
     <div className="col-lg-4 col-md-12 p-md-2 hidden-b">
       <a href="#">
-        <img src="img/cpu_888995.png" alt="" width="50px"/>
+        <img src={photo2} alt="" width="50px"/>
         <p>Generatives AI</p>
       </a>
     </div>
     <div className="col-lg-4 col-md-12 p-md-2 hidden-b">
       <a href="#">
-        <img src="img/3d-model_14189680.png" alt="" width="50px"/>
+        <img src={photo13} alt="" width="50px"/>
         <p>3D Modeling/Rendering</p>
       </a>
     </div>
@@ -54,19 +56,19 @@ export default function Services() {
   <div className="row p-lg-3 p-md-3 p-0">
     <div className="col-lg-4 col-md-12 p-md-2 hidden-1">
       <a href="#">
-        <img src="img/computer_10940047.png" alt="" width="50px"/>
+        <img src={photo}alt="" width="50px"/>
         <p>2D/3D Animation</p>
       </a>
     </div>
     <div className="col-lg-4 col-md-12 p-md-2 hidden-1">
       <a href="#">
-        <img src="img/workplace_8170939.png" alt="" width="50px"/>
+        <img src={photo12} alt="" width="50px"/>
         <p>Motion Graphics</p>
       </a>
     </div>
     <div className="col-lg-4 col-md-12 p-md-2 hidden-1">
       <a href="#">
-        <img src="img/visual-effect_3879988.png" alt="" width="50px"/>
+        <img src={photo10} alt="" width="50px"/>
         <p>VFX</p>
       </a>
     </div>
@@ -74,19 +76,19 @@ export default function Services() {
   <div className="row p-lg-3 p-md-3 p-0">
     <div className="col-lg-4 col-md-12 p-md-2 hidden-1">
       <a href="#">
-        <img src="img/virtual-reality_1377790.png" alt="" width="50px"/>
+        <img src={photo11} alt="" width="50px"/>
         <p>AR/VR/MR/XR</p>
       </a>
     </div>
     <div className="col-lg-4 col-md-12 p-md-r hidden-1">
       <a href="#">
-        <img src="img/studio_9200070.png" alt="" width="50px"/>
+        <img src={photo6} alt="" width="50px"/>
         <p>Virtual Production</p>
       </a>
     </div>
     <div className="col-lg-4 col-md-12 p-md-2 hidden-1">
       <a href="#">
-        <img src="img/production_13136249.png" alt="" width="50px"/>
+        <img src={photo5} alt="" width="50px"/>
         <p>CG</p>
       </a>
     </div>
@@ -94,19 +96,19 @@ export default function Services() {
   <div className="row p-lg-3 p-md-3 p-0">
     <div className="col-lg-4 col-md-12 p-md-2 hidden-1">
       <a href="#">
-        <img src="img/ux-design_7889802.png" alt="" width="50px"/>
+        <img src={photo8} alt="" width="50px"/>
         <p>UI/UX</p>
       </a>
     </div>
     <div className="col-lg-4 col-md-12 p-md-2 hidden-1">
       <a href="#">
-        <img src="img/team_2083290.png" alt="" width="50px"/>
+        <img src={photo7} alt="" width="50px"/>
         <p>Academic Alliances</p>
       </a>
     </div>
     <div className="col-lg-4 col-md-12 p-md-2 hidden-1">
       <a href="#">
-        <img src="img/film-editing_4035077.png" alt="" width="50px"/>
+        <img src={photo3} alt="" width="50px"/>
         <p>Video Editing</p>
       </a>
     </div>
